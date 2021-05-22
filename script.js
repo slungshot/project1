2d77007e983102aaa0c1f2156afe3d1c87227bbf
charpony <charpony@yahoo.com>
2021-05-22 15:17:05 -0500

    initial project 1 commit 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('#clicky');
 const heading = document.querySelector('h1');
 btn.addEventListener('click', function(){
-  heading.innerText += "clicked!";
+  heading.classList.add("red");
+  heading.innerText += " clicked!";
 });
